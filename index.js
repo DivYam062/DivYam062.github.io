676a5ec3bb35d8d4c89d42a1ba3c913b70cc5d23
DivYam062 <divyamrauthan002@gmail.com>
2023-12-22 07:09:58 +0530

    Divyam_Portfolio

--- index.js
+++ index.js
@@ -1,133 +1,100 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu1')
-
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu1')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-// const sections = document.querySelectorAll('section[id]')
-
-// function scrollActive(){
-//     const scrollY = window.pageYOffset
-
-//     sections.forEach(current =>{
-//         const sectionHeight = current.offsetHeight
-//         const sectionTop = current.offsetTop - 50;
-//         sectionId = current.getAttribute('id')
-
-//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-//         }else{
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-//         }
-//     })
-// }
-// window.addEventListener('scroll', scrollActive)
-
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 1000,
-    delay: 100,
-     reset: true
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home-img, .about__subtitle, .about__text, .skills__img',{delay: 100}); 
-sr.reveal('.home__social-icon',{ interval: 100}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 50    }); 
 
-/*SCROLL HOME*/
-sr.reveal( ".home-title", {} );
-// sr.reveal( ".button", { delay: 100 } );
-sr.reveal( ".home-img", { delay: 100 } );
-sr.reveal( ".home-social-icon", { interval: 100 } );
+function openResume() {
+  window.open(
+    "https://drive.google.com/file/d/1Yodc3R_PdfPxjIgUX3Dvr2xtixe_ukKd/view?usp=drive_link",
+    '_blank'
+  );
+}
 
-/*SCROLL ABOUT*/
-sr.reveal( ".about-img", {} );
-sr.reveal( ".about-subtitle", { delay: 100 } );
-sr.reveal( ".about-text", { delay: 100 } );
+function paddingFunction() {
+  let x = doccument.getElementById("home")
+  x.style.padding = "100px"
+}
 
-// Scross main skills mainskills
-sr.reveal( ".mainskills", { interval: 50 } );
-sr.reveal( ".mskill-img", { delay: 50 } );
-sr.reveal( ".mskill-text", { delay: 50 } );
-/*SCROLL SKILLS*/
-sr.reveal( ".skills-subtitle", {} );
-sr.reveal( ".skills-text", {} );
-sr.reveal( ".skills-data", { interval: 50 } );
+document.querySelector("form").addEventListener("submit", saveFile );
 
 
 
-sr.reveal(".contact-input", { interval: 100 });
+function saveFile(e) {
+
+  e.preventDefault()
 
 
-    
-document.querySelector("#resume-button-1").addEventListener("click",()=>{
-    window.open("./pdfs/Divyam_Resume.pdf")
-})
+  let Formdata = JSON.parse(localStorage.getItem("portfolioData")) || []
+
+  let name=document.getElementById("fname").value
+  let email=document.getElementById("email").value
+  let text=document.getElementById("subject").value
+  
+  let data = {
+    "name": name,
+    "email": email,
+    "text": text
+  }
+  Formdata.push(data)
+  localStorage.setItem("portfolioData", JSON.stringify(Formdata))
+}
+
+// Get all the section elements
+// const sections = document.querySelectorAll('section');
+const sections = document.querySelectorAll("#home, #about, #skills, #projects, #github, #contact, #footer");
 
 
-var typing=new Typed(".text", {
-    strings: ["", "FULL-STACK", "WEB", "DEVELOPER"],
-    typeSpeed: 100,
-    backSpeed: 40  ,
-    loop: true,
-});
+// Get all the navigation links
+const navScroll = document.querySelectorAll('#nav-bar a');
 
-let project_NykaaClone = document.getElementById("project_Nykaa-Clone");
-let project_MyntraClone = document.getElementById("project_Myntra-Clone");
+// Function to highlight the current section in the navigation menu
+function highlightNavLink() {
+    let index = sections.length;
 
-let project_NykaaClone_Arr = ["./images/projects/Nykaa_Project/Nykaa_Home"];
-let project_MyntraClone_Arr = ["./images/projects/Myntra_Project/Myntra_Home"];
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
 
-project_NykaaClone.addEventListener("mouseover",()=>{ 
-    let count=0;
-    let intv = setInterval(()=>{
-        if(count<project_NykaaClone_Arr.length-1){
-            count++;
-            project_NykaaClone.src=project_NykaaClone_Arr[count];
-        }else{            
-            count=0;
-            project_NykaaClone.src=project_NykaaClone_Arr[count];
-        }
-    },1000);
+    navScroll.forEach(link => link.classList.remove('active'));
+    navScroll[index].classList.add('active');
+}
 
-    setTimeout(()=>{        
-        clearInterval(intv);
-    },8000);
-})
-project_MyntraClone.addEventListener("mouseover",()=>{ 
-    let count=0;
-    let intv = setInterval(()=>{
-        if(count<project_MyntraClone_Arr.length-1){
-            count++;
-            project_MyntraClone.src=project_MyntraClone_Arr[count];
-        }else{            
-            count=0;
-            project_MyntraClone.src=project_MyntraClone_Arr[count];
-        }
-    },1000);
+// Add event listener for scroll
+window.addEventListener('scroll', highlightNavLink);
 
-    setTimeout(()=>{        
-        clearInterval(intv);
-    },5000);
-})
+
+function typeWriterEffect() {
+  const title = document.getElementById('title');
+  const text = title.innerText;
+  title.innerText = ''; // Clear the text content
+  let charIndex = 0;
+
+  function type() {
+      if (charIndex < text.length) {
+          title.textContent += text.charAt(charIndex);
+          charIndex++;
+          setTimeout(type, 100); // Adjust typing speed (milliseconds)
+      }
+  }
+  type();
+}
+
+// Call the typewriter effect function after AOS animation completes
+function animateAndType() {
+  AOS.init(); // Initialize AOS library
+  AOS.refresh(); // Refresh AOS to apply animations
+
+  setTimeout(typeWriterEffect, 1000); // Adjust delay after AOS animation (milliseconds)
+}
+
+// Trigger typewriter effect after the page loads
+window.addEventListener('load', animateAndType);
